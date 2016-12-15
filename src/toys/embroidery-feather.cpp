a802a8d18693f856e2d67180601a4750ef6a1c2d
Alexander Brock <alexander@lunar-orbit.de>
2016-12-15 17:57:33 +0100

    First steps toward feather embroidery

---
+++ src/toys/embroidery-feather.cpp
@@ -0,0 +1,86 @@
+#include <2geom/d2.h>
+#include <2geom/intersection-graph.h>
+#include <2geom/path.h>
+#include <2geom/sbasis.h>
+#include <2geom/svg-path-parser.h>
+#include <2geom/transforms.h>
+#include <2geom/sbasis-to-bezier.h>
+#include <2geom/svg-path-writer.h>
+#include <2geom/curve.h>
+
+//#include <toys/path-cairo.h>
+//#include <toys/toy-framework-2.h>
+
+//#include <cairo/cairo.h>
+//#include <gtk-2.0/gdk/gdkevents.h>
+
+#include <cstdlib>
+#include <boost/algorithm/minmax_element.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <boost/filesystem.hpp>
+
+#include "helper/geom-pathstroke.h"
+
+#include "RunningStats.h"
+
+using namespace Geom;
+
+
+#include "hair.h"
+
+namespace fs = boost::filesystem;
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " outlinefile curvefile templatefile" << std::endl;
+        return 0;
+    }
+    if (argc > 1) {
+        std::cerr << "Argument 1: " << argv[1] << std::endl;
+    }
+
+    fs::path full_path( fs::initial_path<fs::path>() );
+    std::cerr << "Current path: " << fs::current_path() << std::endl;
+    full_path = fs::system_complete( fs::path( argv[0] ) );
+    std::cerr << full_path << std::endl;
+    //Without file name
+    std::cerr << full_path.stem() << std::endl;
+    fs::current_path(full_path.remove_filename());
+
+
+    const std::string outline_file(argv[1]);
+    const std::string curve_file(argv[2]);
+    const std::string template_file(argv[3]);
+    std::cout << "Outline file: " << outline_file << std::endl
+              << "Curve file: " << curve_file << std::endl;
+    PathVector boundary = read_svgd(outline_file.c_str());
+    PathVector curve = read_svgd(curve_file.c_str());
+    PathVector template_path = read_svgd(template_file.c_str());
+
+    Hair hair;
+
+    hair.setOutline(boundary[0]);
+    hair.setFeatherCurve(curve[0]);
+    hair.setFeatherTemplate(template_path[0]);
+
+    hair.runFeather();
+
+    hair.write(outline_file + ".svg");
+    hair.writeCurves(outline_file + "-curves.svg");
+    hair.writeStitches(hair.greedySolution, outline_file + "-greedy.txt");
+
+    hair.printStats();
+
+    /*
+    hair.writeAreas("tail-1-areas.svg");
+
+    hair.writeForwardAreas("tail-1-forward-areas.svg");
+    hair.writeReverseAreas("tail-1-reverse-areas.svg");
+    */
+
+
+    return 0;
+}
---
+++ src/toys/embroidery-hair.cpp
@@ -0,0 +1,80 @@
+#include <2geom/d2.h>
+#include <2geom/intersection-graph.h>
+#include <2geom/path.h>
+#include <2geom/sbasis.h>
+#include <2geom/svg-path-parser.h>
+#include <2geom/transforms.h>
+#include <2geom/sbasis-to-bezier.h>
+#include <2geom/svg-path-writer.h>
+#include <2geom/curve.h>
+
+//#include <toys/path-cairo.h>
+//#include <toys/toy-framework-2.h>
+
+//#include <cairo/cairo.h>
+//#include <gtk-2.0/gdk/gdkevents.h>
+
+#include <cstdlib>
+#include <boost/algorithm/minmax_element.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <boost/filesystem.hpp>
+
+#include "helper/geom-pathstroke.h"
+
+#include "RunningStats.h"
+
+using namespace Geom;
+
+
+#include "hair.h"
+
+namespace fs = boost::filesystem;
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " outlinefile curvefile" << std::endl;
+        return 0;
+    }
+    if (argc > 1) {
+        std::cerr << "Argument 1: " << argv[1] << std::endl;
+    }
+
+    fs::path full_path( fs::initial_path<fs::path>() );
+    std::cerr << "Current path: " << fs::current_path() << std::endl;
+    full_path = fs::system_complete( fs::path( argv[0] ) );
+    std::cerr << full_path << std::endl;
+    //Without file name
+    std::cerr << full_path.stem() << std::endl;
+    fs::current_path(full_path.remove_filename());
+
+
+    const std::string outline_file(argv[1]);
+    const std::string curve_file(argv[2]);
+    std::cout << "Outline file: " << outline_file << std::endl
+              << "Curve file: " << curve_file << std::endl;
+    PathVector boundary = read_svgd(outline_file.c_str());
+    PathVector curve = read_svgd(curve_file.c_str());
+
+    Hair hair(boundary[0], curve[0]);
+
+
+    hair.run();
+    hair.write(outline_file + ".svg");
+    hair.writeCurves(outline_file + "-curves.svg");
+    hair.writeStitches(hair.greedySolution, outline_file + "-greedy.txt");
+
+    hair.printStats();
+
+    /*
+    hair.writeAreas("tail-1-areas.svg");
+
+    hair.writeForwardAreas("tail-1-forward-areas.svg");
+    hair.writeReverseAreas("tail-1-reverse-areas.svg");
+    */
+
+
+    return 0;
+}
